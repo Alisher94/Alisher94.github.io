1f92d7cfa801ebe52456ec3b84bfa2ea9e6953b1
Alisher94 <a.alisher.abdullayev@gmail.com>
2022-05-13 17:23:25 -0500

    Fifteen Puzzle

---
+++ fifteen_puzzle/fifteen.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+    init();
+    $("#shufflebutton").on("click", shufflePicture);
+
+});
+
+var positionArray = [];
+var  init = function() {
+    var puzzleArea = document.getElementById('puzzlearea');
+    var divs = puzzleArea.getElementsByTagName("div");
+        
+    // initialize each piece
+    for (var i=0; i< divs.length; i++) {
+        var div = divs[i];
+        
+        // calculate x and y for this piece
+        var x = ((i % 4) * 100) ;
+        var y = (Math.floor(i / 4) * 100) ;
+
+        // set basic style and background
+        div.className = "puzzlepiece";
+        div.style.left = x + 'px';
+        div.style.top = y + 'px';
+        div.style.backgroundImage = 'url("background.jpg")';
+        div.style.backgroundPosition = -x + 'px ' + (-y) + 'px';
+        
+        // store x and y for later
+        div.x = x;
+        div.y = y; 
+        var point = {x:x, y:y , bgP:div.style.backgroundPosition };
+        positionArray.push(point);
+    }    
+      
+};
+
+
+
+var shufflePicture  = function () {
+    alert ('shuffle');
+
+    var divElements =  $("#puzzlearea").children('div');
+
+    $("#puzzlearea").children('div').each(function(key, el) {
+        $(el).detach();
+    });
+
+    var shuffledArray = shuffleGivenArray(positionArray);
+    for (var i=0; i< divElements.length; i++) {
+        var item = divElements[i];
+        item.x = shuffledArray[i].x;
+        item.y = shuffledArray[i].y;
+        item.style.left = item.x + 'px';
+        item.style.top  = item.y + 'px';
+        item.style.backgroundPosition = item.bgP;
+        $("#puzzlearea").append(item);
+    }
+};
+
+var shuffleGivenArray = function (arr) {
+    var index = arr.length;
+    var temp;
+    var randIndex;
+
+    while (0 !== index) {
+        // find a remaining item
+        randIndex = Math.floor(Math.random() * index);
+        index -= 1;
+
+        // swap items
+        temp = arr[index];
+        arr[index] = arr[randIndex];
+        arr[randIndex] = temp;
+    }
+
+    return arr;
+};
+
+
